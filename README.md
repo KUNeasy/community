848b6c6f59a8e3eb6df92e9d7a9797a2ed582dd3
KUNeasy <1506714096@qq.com>
2020-03-04 19:52:48 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 [Spring io](https://spring.io)  
 [ElasticSearch中文社区](https://elasticsearch.cn)   
 [Github授权App](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
-
+[okhttp](https://square.github.io/okhttp/)  
 ## 工具
 [Git工具](https://git-scm.com/downloads)  
+[visual Paradigm](https://www.visual-paradigm.com/cn)
--- src/main/java/com/mrkun/community/controller/IndexController.java
+++ src/main/java/com/mrkun/community/controller/IndexController.java
@@ -1,18 +1,16 @@
-package com.mrkun.community.hellocontroller;
+package com.mrkun.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author by liukun
  * @date 2020/3/4
  */
 @Controller
-public class HelloController {
+public class IndexController {
     @GetMapping("/")
-    public String hello(){
+    public String index(){
         return "index";
     }
 
